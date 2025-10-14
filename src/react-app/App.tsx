f0cfb8d50aad8760d02d3392f953bd773b78564a
Nikolay Gospodinov <Nikolay.Gospodinov@a1.bg>
2025-10-14 15:49:00 +0300

    .

--- src/react-app/App.tsx
+++ src/react-app/App.tsx
@@ -1,10 +1,6 @@
 // src/App.tsx
 
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import cloudflareLogo from "./assets/Cloudflare_Logo.svg";
-import honoLogo from "./assets/hono.svg";
 import "./App.css";
 
 function App() {
@@ -13,24 +9,6 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-        <a href="https://hono.dev/" target="_blank">
-          <img src={honoLogo} className="logo cloudflare" alt="Hono logo" />
-        </a>
-        {/* <a href="https://workers.cloudflare.com/" target="_blank">
-          <img
-            src={cloudflareLogo}
-            className="logo cloudflare"
-            alt="Cloudflare logo"
-          />
-        </a> */}
-      </div>
       <h1>TEST</h1>
       <div className="card">
         <button
@@ -54,11 +32,7 @@ function App() {
         >
           Name from API is: {name}
         </button>
-        <p>
-          Edit <code>worker/index.ts</code> to change the name
-        </p>
       </div>
-      <p className="read-the-docs">Click on the logos to learn more</p>
     </>
   );
 }
